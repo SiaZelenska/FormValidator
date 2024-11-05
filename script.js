7faa03eb930bce22aa73e9d1b36a16c7523d4998
мак <mak@MacBook-Air-Matvej.local>
2024-11-05 19:04:53 +0100

    WIp

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
 const form = document.querySelector(".form");
 
-form.addEventListener("submit",(e)=> {
-    e.preventDefault();
-    let inputName = document.querySelector(".name");
-    let inputEmail = document.querySelector(".email");
+const formClasses = {
+  error: "error",
+  success: "success",
+};
 
-    validate(form);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let inputName = document.querySelector(".name");
+  let inputEmail = document.querySelector(".email");
+
+  validate(form);
 });
 
 
-function validate (form) {
-    let inputs = document.querySelectorAll("input");
-
-    const inputRegex = /^[A-Za-z]+$/;
-    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/;
-    
-    inputs.forEach((input)=> {
-
-        const value = input.value;
-      console.log(emailRegex.test(value))
-
-        if (input.type == "email") {
-            lightInput(input, emailRegex.test(value));
-        } else {
-
-            lightInput(input, inputRegex.test(input));
-        }
-    })
+function validate(form) {
+  let inputs = document.querySelectorAll("input");
+  inputs.forEach((input) => {
+    validateField(input);
+  })
 }
 
-function lightInput(input, corrected){
-    (corrected) ? input.style.border = "2px solid green" : input.style.border = "2px solid red";
+function validateField(field) {
+  const inputRegex = /^[A-Za-z]+$/;
+  const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/;
+
+  const value = field.value;
+  const type = field.type;
+
+  if (!value) {
+    setFieldError(field);
+  }
+}
+
+function setFieldError(field) {
+  field.classList.add(formClasses.error);
+  field.classList.remove(formClasses.success);
+}
+
+function setFieldSuccess(field) {
+  field.classList.add(formClasses.success);
+  field.classList.remove(formClasses.error);
 }
